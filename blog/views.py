8233cfe95ba7c86f69fd6ad23779b9544006fcf5
bomnarae <bomnarae@dini.io>
2019-09-27 18:07:43 +0700

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -11,3 +12,42 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        print(form)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    # Create a form to edit an existing Article,
+    # but use POST data to populate the form
+    if request.method == "POST":
+        #  참고: https://tutorial.djangogirls.org/ko/django_forms/#%ED%8F%BC-%EC%88%98%EC%A0%95%ED%95%98%EA%B8%B0
+        # form = PostForm(request.POST, instance=post)를 form = PostForm(instance=post)로 수정????
+        form = PostForm(request.POST, instance=post)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm(instance=post)
+        print(form.is_valid())
+
+    return render(request, 'blog/post_edit.html', {'form': form})
